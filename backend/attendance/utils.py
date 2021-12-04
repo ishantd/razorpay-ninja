e34cfb0b26765a77ca3aed286a9a274fd401b14f
Ishant Dahiya <ishantdahiya@gmail.com>
2021-12-04 23:26:33 +0530

    apr get distance

--- backend/attendance/utils.py
+++ backend/attendance/utils.py
@@ -46,6 +46,15 @@ class ProcessAttendance:
         distance = int(haversine((in_location.latitude, in_location.longitude), (out_location.latitude, out_location.longitude), unit=Unit.METERS))
         return distance <= settings.MAX_DISTANCE
 
+    def get_distance(self):
+        """
+        Returns the distance between the employee's location
+        """
+        in_location = self.attendance.attendance_time_in_location
+        out_location = self.attendance.attendance_time_out_location
+        distance = int(haversine((in_location.latitude, in_location.longitude), (out_location.latitude, out_location.longitude), unit=Unit.KILOMETERS))
+        return distance
+
 def compare_faces_for_checkin(target, source):
     response = client.compare_faces(
     SourceImage={
